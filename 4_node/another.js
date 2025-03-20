8e2a2c52f57f034d43dc0c298423c85ad15620ee
zafar2154 <zafar2154.ez@gmail.com>
2025-03-20 17:51:14 +0700

    nambahin materi 4

--- 4_node/another.js
+++ 4_node/another.js
@@ -1,1 +1,12 @@
-nama = (nama) => {console.log(`aku adalah ${nama}`)}
+// nama = (name) => `aku adalah ${name}`;
+// umur = (umur) => `umur aku ${umur}`;
+// nim = (nim) => `nim : ${nim}`;
+// x = 10;
+// const durian = {
+//     warna: 'kuning',
+//     umur: 4,
+//     jenis: 'musang king',
+//     cetakDurian: function () { `ini durian jenis ${this.jenis}, berumur ${this.umur} tahun, warna ${this.warna}.` }
+// }
+
+// module.exports = {nama, umur, durian, nim};
--- 4_node/script.js
+++ 4_node/script.js
@@ -1,2 +1,31 @@
-var impor = require('another.js');
-impor.nama('eza');
+// //local modules
+// const another = require('./another');
+
+// console.log(nama('eza'));
+// console.log(umur(19));
+// console.log(x);
+// console.log(another.durian.cetakDurian());
+
+
+// //core modules
+// const http = require('http');
+// http.createServer(function (req, res) {
+//     res.writeHead(200, { 'Content-Type': 'text/html' });
+//     switch(req.url){
+//         case '/about':
+//             res.write('ini adalah halaman about');
+//             break;
+//         default:
+//             res.write('error 404 not found');
+//     }
+//     res.end();
+// }).listen(8080);
+
+// //NPM
+// const express = require('express');
+// const app = express();
+// app.get('/', (req, res) => {
+//     res.send('Hello, Express!');
+// });
+
+// app.listen(3000, () => console.log('Server berjalan di http://localhost:3000'));
